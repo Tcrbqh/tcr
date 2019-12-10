33e2e72db1b76f35c62bcc2276feb3cba9e89124
Tcrbqh <54838840+Tcrbqh@users.noreply.github.com>
2019-12-10 17:55:21 +0800

    Update and rename java book 7-1.java to Java book 7-1AnalyzeNubers.java

--- Java book 7-1AnalyzeNubers.java
+++ Java book 7-1AnalyzeNubers.java
@@ -1,5 +1,5 @@
 
-public class tcr {
+public class AnalyzeNumbers {
 	public static void main(String[] args) {
 		java.util.Scanner input=new java.util.Scanner(System.in);
 		System.out.print("Enter the number of items: ");
